1f593f05f6f2e175add8d385f7c6bbd8bd7cc6d3
Gallegos19 <223204@ids.upchiapas.edu.mx>
2024-01-21 18:57:37 -0600

    Implementacion del socketIo

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv';
 import express from "express";
 import indexRoute from './src/routes/index.route.js';
 import database from './src/config/database.js';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import ConectarSocket from './src/middleware/socketio/socketIo.js';
+
 
 const app = express();
 app.use(cors());
@@ -10,8 +14,15 @@ app.use(express.json());
 app.use("/",indexRoute);
 dotenv.config();
 app.set("PORT", process.env.PORT || 3003);
-app.listen(app.get("PORT"),() => {console.log("listening on port "+app.get("PORT"));});
-app.use("*", (req,res)=>{  res.status(404).send('No está mi mike');});
+const server = createServer(app);
+ConectarSocket(server);
+const PORT = process.env.PORT || 3001;
+
+
+server.listen(PORT, () => {
+    console.log(`Servidor y Socket.IO en ejecución en http://localhost:${PORT}`);
+  });
+  app.use("*", (req,res)=>{  res.status(404).send('No está mi mike');});
 database.connect()
 .then(()=>{console.log("database connected")})
 .catch((error) => { console.log(error)})
